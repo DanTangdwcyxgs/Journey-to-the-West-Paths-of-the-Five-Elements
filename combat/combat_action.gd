c83c5d6031da741590543409ce0d23703604faf7
蛋汤 <dantangdwcyxgs@gmail.com>
2026-09-04 14:01:26 +0800

    feat: add data-driven combat action model

---
+++ combat/combat_action.gd
@@ -0,0 +1,24 @@
+class_name CombatAction
+extends RefCounted
+
+var id: String
+var display_name: String
+var element: String
+var power: int
+var shield_hit: int
+var bp_cost: int
+
+func _init(
+	p_id: String,
+	p_display_name: String,
+	p_element: String,
+	p_power: int,
+	p_shield_hit: int = 0,
+	p_bp_cost: int = 0
+) -> void:
+	id = p_id
+	display_name = p_display_name
+	element = p_element
+	power = p_power
+	shield_hit = p_shield_hit
+	bp_cost = p_bp_cost
