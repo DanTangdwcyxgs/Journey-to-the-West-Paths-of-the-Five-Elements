2a824911c1d16bccef1dec0331506c9aaff752d4
蛋汤 <dantangdwcyxgs@gmail.com>
2026-09-04 16:58:17 +0800

    test: align encounter weakness keys with normalized runtime

--- combat/test_encounter_manager.gd
+++ combat/test_encounter_manager.gd
@@ -1,7 +1,6 @@
 extends RefCounted
 
 ## Regression checks for normal encounter data loading and enemy construction.
-
 static func run() -> void:
 	var manager := EncounterManager.new()
 	var definition := manager.get_definition("YELLOW_WIND_CAVE_SAND_GUARDS")
@@ -10,6 +9,7 @@ static func run() -> void:
 	var enemies := manager.build_enemies("YELLOW_WIND_CAVE_SAND_GUARDS")
 	assert(enemies.size() == 2)
 	assert(enemies[0].id == "sand_guard")
-	assert(enemies[0].weaknesses.get("FIRE", false))
+	assert(enemies[0].weaknesses.get("fire", false))
 	assert(enemies[1].id == "wind_spirit")
-	assert(enemies[1].weaknesses.get("WATER", false))
+	assert(enemies[1].weaknesses.get("water", false))
+	assert(not enemies[0].combat_modifiers.get("skills", []).is_empty())
