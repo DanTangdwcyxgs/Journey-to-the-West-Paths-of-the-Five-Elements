0859a618faed84d48ba70a39b552881779812983
蛋汤 <dantangdwcyxgs@gmail.com>
2026-09-04 16:35:20 +0800

    test: cover data driven origin combat traits

--- combat/test_origin_choice_combat.gd
+++ combat/test_origin_choice_combat.gd
@@ -4,20 +4,20 @@ extends RefCounted
 
 static func run() -> void:
 	var power_choice := Combatant.new("wukong", "孙悟空", 100, 20, 5, 12, 2, {})
-	CombatPartyBuilder._apply_origin_choices(power_choice, {"WUK-03":"SEEK_POWER"})
+	CombatPartyBuilder._apply_origin_choices(power_choice, "WUKONG", {"WUK-03":"SEEK_POWER"})
 	assert(power_choice.attack == 22)
 
 	var freedom_choice := Combatant.new("wukong", "孙悟空", 100, 20, 5, 12, 2, {})
-	CombatPartyBuilder._apply_origin_choices(freedom_choice, {"WUK-08":"REJECT_BINDING"})
+	CombatPartyBuilder._apply_origin_choices(freedom_choice, "WUKONG", {"WUK-08":"REJECT_BINDING"})
 	assert(freedom_choice.speed == 13)
 	assert(freedom_choice.base_speed == 13)
 
 	var endure_choice := Combatant.new("wukong", "孙悟空", 100, 20, 5, 12, 2, {})
-	CombatPartyBuilder._apply_origin_choices(endure_choice, {"WUK-13":"ENDURE"})
+	CombatPartyBuilder._apply_origin_choices(endure_choice, "WUKONG", {"WUK-13":"ENDURE"})
 	assert(endure_choice.defense == 7)
 
 	var combined := Combatant.new("wukong", "孙悟空", 100, 20, 5, 12, 2, {})
-	CombatPartyBuilder._apply_origin_choices(combined, {
+	CombatPartyBuilder._apply_origin_choices(combined, "WUKONG", {
 		"WUK-03":"SEEK_POWER",
 		"WUK-08":"REJECT_BINDING",
 		"WUK-13":"ENDURE"
@@ -26,3 +26,12 @@ static func run() -> void:
 	assert(combined.speed == 13)
 	assert(combined.base_speed == 13)
 	assert(combined.defense == 7)
+
+	var tang := Combatant.new("tangseng", "唐三藏", 100, 10, 5, 8, 2, {})
+	CombatPartyBuilder._apply_origin_choices(tang, "TANG", {"TANG-04":"SERVE_PEOPLE"})
+	assert(is_equal_approx(float(tang.combat_modifiers.get("healing_multiplier", 1.0)), 1.15))
+
+	var longma := Combatant.new("longma", "白龙马", 100, 14, 5, 12, 2, {})
+	CombatPartyBuilder._apply_origin_choices(longma, "LONGMA", {"LONGMA-02":"RESIST_FATE"})
+	assert(longma.speed == 14)
+	assert(longma.base_speed == 14)
