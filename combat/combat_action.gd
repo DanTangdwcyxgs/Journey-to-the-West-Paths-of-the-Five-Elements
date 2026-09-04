4ce9bf201938299f6d118926a68c38a504a90189
蛋汤 <dantangdwcyxgs@gmail.com>
2026-09-04 17:17:54 +0800

    feat: attach data-driven enemy skill effects to combat actions

--- combat/combat_action.gd
+++ combat/combat_action.gd
@@ -7,6 +7,7 @@ var element: String
 var power: int
 var shield_hit: int
 var bp_cost: int
+var effects: Dictionary = {}
 
 func _init(
 	p_id: String,
@@ -14,7 +15,8 @@ func _init(
 	p_element: String,
 	p_power: int,
 	p_shield_hit: int = 0,
-	p_bp_cost: int = 0
+	p_bp_cost: int = 0,
+	p_effects: Dictionary = {}
 ) -> void:
 	id = p_id
 	display_name = p_display_name
@@ -22,3 +24,4 @@ func _init(
 	power = p_power
 	shield_hit = p_shield_hit
 	bp_cost = p_bp_cost
+	effects = p_effects.duplicate(true)
