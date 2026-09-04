d1cceed0e1814221d2dd9cda7ccef16e5992413d
蛋汤 <dantangdwcyxgs@gmail.com>
2026-09-04 16:07:05 +0800

    test: cover data-driven Yellow Wind Cave encounter construction

---
+++ combat/test_encounter_manager.gd
@@ -0,0 +1,15 @@
+extends RefCounted
+
+## Regression checks for normal encounter data loading and enemy construction.
+
+static func run() -> void:
+	var manager := EncounterManager.new()
+	var definition := manager.get_definition("YELLOW_WIND_CAVE_SAND_GUARDS")
+	assert(not definition.is_empty())
+	assert(definition.get("recommended_level", 0) == 10)
+	var enemies := manager.build_enemies("YELLOW_WIND_CAVE_SAND_GUARDS")
+	assert(enemies.size() == 2)
+	assert(enemies[0].id == "sand_guard")
+	assert(enemies[0].weaknesses.get("FIRE", false))
+	assert(enemies[1].id == "wind_spirit")
+	assert(enemies[1].weaknesses.get("WATER", false))
