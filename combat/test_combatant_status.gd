8ded0158359c1c8907dfa2fa99777c18aa500c01
蛋汤 <dantangdwcyxgs@gmail.com>
2026-09-04 15:42:51 +0800

    Add combat status summary regression checks

---
+++ combat/test_combatant_status.gd
@@ -0,0 +1,15 @@
+extends RefCounted
+
+## Regression checks for the readable temporary combat status layer.
+
+static func run() -> void:
+	var unit := Combatant.new("test", "测试", 100, 20, 10, 10, 2, {})
+	assert(unit.get_status_summary() == "无状态")
+	unit.gain_barrier(20)
+	assert("护盾 +20" in unit.get_status_summary())
+	unit.apply_speed_delta(-5, 2)
+	unit.aggro_turns = 1
+	assert("速度 -5 2T" in unit.get_status_summary())
+	assert("嘲讽 1T" in unit.get_status_summary())
+	unit.broken_turns = 2
+	assert("Break 2T" in unit.get_status_summary())
