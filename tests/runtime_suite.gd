c1591d0d844f1d57936c34031bd1185f3ca26d1d
蛋汤 <dantangdwcyxgs@gmail.com>
2026-09-05 11:43:41 +0800

    test(godot): cover narrative event session runtime

--- tests/runtime_suite.gd
+++ tests/runtime_suite.gd
@@ -7,6 +7,7 @@ const TEST_SCRIPTS := [
 	"res://combat/test_chapter_runtime.gd",
 	"res://combat/test_event_runtime.gd",
 	"res://combat/test_event_runner.gd",
+	"res://combat/test_narrative_event_session.gd",
 	"res://combat/test_shared_encounter_combat.gd",
 	"res://combat/test_shared_journey_battles.gd",
 	"res://combat/test_battle_resolution_service.gd",
